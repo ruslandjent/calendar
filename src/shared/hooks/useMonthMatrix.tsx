f250e90072cce589db37572e2deb78abac2c27ba
ruslandjent <sayfulinruslan.djnt@gmail.com>
2020-09-21 16:59:27 +0500

    animation bug fixed

--- src/shared/hooks/useMonthMatrix.tsx
+++ src/shared/hooks/useMonthMatrix.tsx
@@ -1,10 +1,13 @@
 import {useState, useEffect} from 'react';
+import {animationDelay} from '../defaults';
 import {generateMonthMatrix, MonthMatrix} from '../functions/generateMonthMatrix';
 
 export const useMonthMatrix = (year: number, month: number): MonthMatrix[] => {
   const [matrix, setMatrix] = useState<MonthMatrix>(generateMonthMatrix(year, month));
 
-  useEffect(() => setMatrix(generateMonthMatrix(year, month)), [year, month]);
+  useEffect(() => {
+    setTimeout(() => setMatrix(generateMonthMatrix(year, month)), animationDelay / 2);
+  }, [year, month]);
 
   return [matrix];
 };
